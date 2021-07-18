9e24de34e7a75dbc01497b153bdff2f10649006d
Gerry Sihaj <gsihaj5@gmail.com>
2021-07-18 11:07:28 +0700

    harvester now can store to container

--- harvester.js
+++ harvester.js
@@ -31,8 +31,16 @@ class Harvester {
         let targetStorage = storageAndPath.target
 
         for (const resourceType in this.creep.carry) {
-            if (this.creep.transfer(targetStorage, resourceType) === ERR_NOT_IN_RANGE)
-                this.creep.moveByPath(storagePath);
+            let transferStatus = this.creep.transfer(targetStorage, resourceType)
+            console.log(transferStatus)
+            switch (transferStatus) {
+                case ERR_NOT_IN_RANGE :
+                    this.creep.moveByPath(storagePath);
+                    break;
+                case ERR_FULL :
+                    this.creep.memory.pathToStorage = "[]";
+            }
+
         }
     }
 
@@ -55,8 +63,9 @@ class Harvester {
     }
 
     findStorageAndPath() {
-        this.findStorageStructure();
-        const targetStorage = Game.getObjectById(this.creep.memory.storeTarget);
+        let structure_id = this.findStorageStructure();
+        this.creep.memory.storeTarget = structure_id;
+        const targetStorage = Game.getObjectById(structure_id);
 
         if (!this.creep.memory.pathToStorage || JSON.parse(this.creep.memory.pathToStorage).length === 0) {
             const pathToTarget = this.creep.pos.findPathTo(targetStorage);
@@ -70,17 +79,34 @@ class Harvester {
     }
 
     findStorageStructure() {
-        let myStructures = this.creep.room.find(FIND_MY_STRUCTURES, {
-            filter: function (object) {
-                // if (object.store != null) {
-                //     console.log(object.store.getCapacity(RESOURCE_ENERGY));
-                // }
+        let myStructures = this.findMyStructures()
+        if (myStructures.length > 0) return myStructures[0].id;
 
-                return object.store != null;
+        myStructures = this.findNeutralStorageStructures()
+        if (myStructures.length > 0) return myStructures[0].id;
+    }
+
+    findMyStructures() {
+        return this.creep.room.find(FIND_MY_STRUCTURES, {
+            filter: function (structure) {
+                let type = structure.structureType
+                if (type === STRUCTURE_CONTROLLER) return;
+
+                return structure.store.getFreeCapacity[RESOURCE_ENERGY] > 0
             }
         });
-        this.creep.memory.storeTarget = myStructures[0].id;
+    }
 
+    findNeutralStorageStructures() {
+        return this.creep.room.find(FIND_STRUCTURES, {
+            filter: function (structure) {
+                let type = structure.structureType
+
+                if (!(type === STRUCTURE_CONTAINER)) return;
+
+                return structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            }
+        });
     }
 
     chooseTargetResource() {
